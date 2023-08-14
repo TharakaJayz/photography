4164a6f22949a158353a3d0c526465cad248bea0
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-08-14 13:26:46 +0530

    second test commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>
-      this is app js
-    </div>
-  )
-}
+  return <div>this is app jssdfsfsdf</div>;
+};
 
-export default App
+export default App;
