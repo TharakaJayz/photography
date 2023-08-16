81602d584b9fa6ca3efe29da683081fd13a795ea
TharakaJayz <117371301+TharakaJayz@users.noreply.github.com>
2023-08-16 23:07:20 +0530

    finished navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,45 +1,62 @@
-import React, { useState } from 'react';
-import './Navbar.css';
-import {AiOutlinePlus}  from 'react-icons/ai';
-import NavCard from './navCard/NavSec2Card';
-import NavSec2Card from './navCard/NavSec2Card';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import NavCard from "./navCard/NavSec2Card";
+import NavSec2Card from "./navCard/NavSec2Card";
 const Navbar = () => {
-
-  const [menuLogic,setMenuLogic] = useState(false);
+  const [menuLogic, setMenuLogic] = useState(false);
   // const menuHandler = ()  =>{
 
   // }
+
+  const recipient = 'tharakaprabhath300@gmail.com';
+  const subject = "Hello from your Website";
+  const body = 'I just wanted to say hello!';
+
+  const emailLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+
   return (
-    <div className = "nav-main">
+    <div className="nav-main">
       <section className="nav-sec nav-sec--1">
-        <div className='nav-sec--1-div_1'>
-          <AiOutlinePlus/>
+        <div className="nav-sec--1-div_1">
+          <AiOutlinePlus />
           FOLLOW
         </div>
-        <div className='nav-sec--1-div_2'>
+        <div className="nav-sec--1-div_2">
           AMELIA
           <span>WEDDING PHOTOGRAPHER</span>
         </div>
-        <div className='nav-sec--1-div_3'  onClick={  ()  =>{
-          setMenuLogic(!menuLogic);
-
-        }}>
-          Menu <section className='nav-sec--1-div_3-sec'>
-            <span className='nav-sec--1-div_3-sec-sp sp1'></span>
-            <span className='nav-sec--1-div_3-sec-sp sp2'></span>
+        <div
+          className="nav-sec--1-div_3"
+          onClick={() => {
+            setMenuLogic(!menuLogic);
+          }}
+        >
+          Menu{" "}
+          <section className="nav-sec--1-div_3-sec">
+            <span className="nav-sec--1-div_3-sec-sp sp1"></span>
+            <span className="nav-sec--1-div_3-sec-sp sp2"></span>
           </section>
         </div>
       </section>
-      <section className = { menuLogic ? 'nav-sec-b display'  : 'nav-sec-b'} >
-      <section className="nav-sec nav-sec--2">
-          <NavSec2Card  />
-      </section>
-      <section className="nav-sec nav-sec--3"></section>
+      <section className={menuLogic ? "nav-sec-b display" : "nav-sec-b"}>
+        <section className="nav-sec nav-sec--2">
+          <NavSec2Card />
+        </section>
+        <section className="nav-sec nav-sec--3">
+          <h3 className="nav-sec--3-h4-1">Let's Get in Touch</h3>
+          <p className="nav-sec--3-p">Petat aut petat aut perferendis doloribus asperiores repellat hanc ego assentior, cum a philosophis compluribus permulta dicantur, cur verear.</p>
+          <h4 className="nav-sec--3-h4-2"><a  href={emailLink}>tharakaprabhath300@gmail.com</a> </h4>
+          <h4 className="nav-sec--3-h4-3">+94 718736614</h4>
+          <button className="nav-sec--3-btn">Request a quote</button>
+
+        </section>
       </section>
 
       {/* <NavCard  className = { menuLogic ? 'nav-card display'  : 'nav-card'} /> */}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/components/Navbar/navCard/NavSec2Card.js
+++ client/src/components/Navbar/navCard/NavSec2Card.js
@@ -1,18 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavSec2Card.css";
-
 const NavSec2Card = (props) => {
-  return <div className={`navSec2Card-main ${props.className}`}>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
-    <span className="navSec2Card-main-sp sec2Card-sp1"></span>
+  const [classStyle, setClassStyle] = useState("");
 
-
-    
-     </div>;
+  return (
+    <div className={`navSec2Card-main ${props.className} ${classStyle}  `}>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp1"
+        onMouseEnter={() => {
+          setClassStyle("home-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("home-hove-leave");
+        }}
+      >
+        Home
+      </span>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp2"
+        onMouseEnter={() => {
+          setClassStyle("gallery-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("gallery-hove-leave");
+        }}
+      >
+        Gallery
+      </span>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp3"
+        onMouseEnter={() => {
+          setClassStyle("Services-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("Services-hove-leave");
+        }}
+      >
+        Services
+      </span>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp4"
+        onMouseEnter={() => {
+          setClassStyle("Blog-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("Blog-hove-leave");
+        }}
+      >
+        Blog
+      </span>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp5"
+        onMouseEnter={() => {
+          setClassStyle("home-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("home-hove-leave");
+        }}
+      >
+        About
+      </span>
+      <span
+        className="navSec2Card-main-sp sec2Card-sp6"
+        onMouseEnter={() => {
+          setClassStyle("Contact-hover");
+        }}
+        onMouseLeave={() => {
+          setClassStyle("Contact-hove-leave");
+        }}
+      >
+        Contact
+      </span>
+    </div>
+  );
 };
 
 export default NavSec2Card;
